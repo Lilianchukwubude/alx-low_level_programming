c75ade8ba3fae2166b533f2c91e12b385185f169
Lilianchukwubude <lilianchukwubude@gmail.com>
2022-09-19 06:58:06 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,28 +15,28 @@
 int _atoi(char *s)
 {
 	int i;
-	int x, y;
+	int h, p;
 
 	h = 0;
 	p = -1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-			y *= -1;
+			p *= -1;
 
 		if (s[i] > 47 && s[i] < 58)
 		{
-			if (x < 0)
-				x = (x * 10) - (s[i] - '0');
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
 			else
-				x = (s[i] - '0') * -1;
+				h = (s[i] - '0') * -1;
 
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
 	}
-	if (y < 0)
-		x *= -1;
+	if (p < 0)
+		h *= -1;
 
-	return (x);
+	return (h);
 }
