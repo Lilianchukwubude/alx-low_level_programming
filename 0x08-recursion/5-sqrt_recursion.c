1e3407fd78f066e4b21d46361c5d35f76d2291a5
Lilianchukwubude <lilianchukwubude@gmail.com>
2022-09-28 06:52:13 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,36 @@
 #include "main.h"
-#include <math.h>
-int _sqrt(int num, int root);
-int _sqrt_recursion(int n);
 
 /**
- * _sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
- *
- * Return: If the number has a natural square root - the square root.
- *         If the number does not have a natural square root - -1.
- */
-int _sqrt(int num, int root)
+* sqrt_check - Checks for the square root of c
+*
+* @g: Guess at sqrt
+*
+* @c: Number to find sqrt of
+*
+* Return: -1 or sqrt of c
+*/
+
+int sqrt_check(int g, int c)
 {
-	if ((root * root) == num)
-		return (root);
-	if (root == num / 2)
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
 		return (-1);
-	return (_sqrt(num, root + 1));
+	return (sqrt_check(g + 1, c));
 }
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to return the square root of.
- * Return: If n has a natural square root - the natural square root of n.
- * If n does not have a natural square root - -1.
- */
+* _sqrt_recursion - Returns the natural square of a number
+*
+* @n: integer to find sqrt of
+*
+* Return: Natural square root or -1
+*/
+
 int _sqrt_recursion(int n)
 {
-	int root = 0;
+	if (n == 0)
+		return (0);
 
-	if (n < 0)
-		return (-1);
-	if (n == 1)
-		return (1);
-	return (_sqrt(n, root));
+	return (sqrt_check(1, n));
 }
-
