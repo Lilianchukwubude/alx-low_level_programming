52583d81e70abd20a4a74550b5b3e99ece63660d
Lilianchukwubude <lilianchukwubude@gmail.com>
2022-12-12 05:56:57 -0800

    math

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,16 @@
+ (15 sloc)  198 Bytes
+#include "main.h"
+/**
+* mod - adds two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of the two integers
+*/
+int mod(int a, int b)
+{
+int mod;
+mod = a % b;
+return (mod);
+}
